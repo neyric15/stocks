37bd605c178472556088ae2daf5de0981cd00db9
Cristiano <developcris2013@gmail.com>
2018-04-20 21:35:50 +0200

    Devise and bootstrap

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,13 @@
+class RegistrationsController < Devise::RegistrationsController
+
+	private
+
+	def sign_up_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :f_name, :l_name, :cell_no)
+	end
+
+	def account_update_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password, :f_name, :l_name, :cell_no)
+	end
+
+end
